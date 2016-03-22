f8ba699ced8d576f2a54c2801ef785b5313841d3
LiYunpeng <liyunpeng@live.com>
2016-03-22 18:02:49 +0800

    为select2增加失去焦点事件，失去焦点后关闭dropdown

--- z-angular-select.js
+++ z-angular-select.js
@@ -2,48 +2,58 @@
  * 基于select2
  */
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('z.angular.select', [])
+    angular
+        .module('z.angular.select', [])
 
-    .directive('zSelect', ['$templateCache','$compile',function ($templateCache,$compile) {
-      return {
-        restrict: 'A',
-        //require:'ngModel',
-        scope: {
-          //multiple:'@'
-        },
-        link: function (scope, element, attrs, controller) {
+        .directive('zSelect', ['$timeout',function ($timeout) {
+            return {
+                restrict: 'A',
+                require:'ngModel',
+                scope: {
+                    //multiple:'@'
+                },
+                link: function (scope, element, attrs, controller) {
 
-          //scope.ngModel = "";
+                    scope.ngModel = "";
+                    scope.select2 = null;
 
-          var select2Option = {
-            //placeholder: "Select a state"
-            width: '100%',
-            //allowClear: true,
-            tags:false
-          };
+                    var select2Option = {
+                        //placeholder: "Select a state"
+                        width: '100%',
+                        //allowClear: true,
+                        tags:false
+                    };
 
-            //隐藏下拉菜单中的搜索框
-            if(attrs.minimumResultsForSearch) {
-                select2Option.minimumResultsForSearch = attrs.minimumResultsForSearch;
-            }
+                    //隐藏下拉菜单中的搜索框
+                    if(attrs.minimumResultsForSearch) {
+                        select2Option.minimumResultsForSearch = attrs.minimumResultsForSearch;
+                    }
 
-          if(element.is('select')) {
-            element.select2(select2Option);
-          } else {
-            throw error("此指令需是用在select标签上");
-          }
+                    if(element.is('select')) {
+                        scope.select2 = element.select2(select2Option);
+                        scope.select2.on('select2-blur',function() {
+                            debugger;
+                        });
+                    } else {
+                        throw error("此指令需是用在select标签上");
+                    }
 
-          if(attrs.multiple) {
-            if(attrs.tags==="true") {
-            } else {
-              element.parent().addClass("hidden-select2");
-            }
-          }
+                    $timeout(function() {
+                        $(element.parent().find('.select2-selection')).on('blur',function() {
+                            element.select2('close');
+                        });
+                    });
 
-        }
-      };
-    }]);
+                    if(attrs.multiple) {
+                        if(attrs.tags==="true") {
+                        } else {
+                            element.parent().addClass("hidden-select2");
+                        }
+                    }
+
+                }
+            };
+        }]);
 }());
