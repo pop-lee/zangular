20cb9bddb8e5b6489f309bb19b81b47e1cef5059
LiYunpeng <liyunpeng@live.com>
2016-04-19 20:05:31 +0800

    移除z-angular-ui-tree，提取树形组件，移动至https://github.com/pop-lee/z-angular-tree

--- z-angular-ui.js
+++ z-angular-ui.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  angular.module('z.angular.ui',['z.angular.ui.tpls', 'z.angular.ui.tree', 'z.angular.loading', 'z.angular.layout','z.angular.viewstack','z.angular.select','z.angular.pagination','z.angular.zoom']);
+  angular.module('z.angular.ui',['z.angular.ui.tpls', 'z.angular.loading', 'z.angular.layout','z.angular.viewstack','z.angular.select','z.angular.pagination','z.angular.zoom']);
   angular.module('z.angular',['z.angular.ui', 'z.angular.util']);
 
 
