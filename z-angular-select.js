52e4dd7f9233199c495d1236489a9dc0b64b9d54
YangFan <yfyhsoft@hotmail.com>
2018-11-27 13:27:46 +0800

    当z-select应用form-control类时，延迟初始化select2（伴随hover事件）

--- z-angular-select.js
+++ z-angular-select.js
@@ -7,46 +7,63 @@
     angular
         .module('z.angular.select', [])
 
-        .directive('zSelect', ['$timeout',function ($timeout) {
+        .directive('zSelect', ['$timeout', function ($timeout) {
             return {
                 restrict: 'A',
-                require:'ngModel',
+                require: 'ngModel',
                 scope: {
-                    ngModel:'='
+                    ngModel: '='
                     //multiple:'@'
                 },
                 link: function (scope, element, attrs, controller) {
 
-                    scope.$on("$destroy",function() {
+                    scope.$on("$destroy", function () {
                         element.prop("disabled", true);
                         //element.select2('close');
-                        scope.select2.trigger("select2:close");
+                        if(scope.select2) {
+                            scope.select2.trigger("select2:close");
+                        }
                     });
 
                     //scope.ngModel = "";
                     scope.select2 = null;
 
-                    $timeout(function() {
-                        scope.select2.val(scope.ngModel);
-                        scope.select2.trigger("change");
+                    $timeout(function () {
+                        if(scope.select2) {
+                            scope.select2.val(scope.ngModel);
+                            scope.select2.trigger("change");
+                        }else{
+                            element.val(scope.ngModel);
+                            element.trigger("change");
+                        }
                     });
 
                     var select2Option = {
                         //placeholder: "Select a state"
                         width: '100%',
                         //allowClear: true,
-                        tags:false
+                        tags: false
                     };
 
                     //隐藏下拉菜单中的搜索框
-                    if(attrs.minimumResultsForSearch) {
+                    if (attrs.minimumResultsForSearch) {
                         select2Option.minimumResultsForSearch = attrs.minimumResultsForSearch;
                     }
 
-                    if(element.is('select')) {
-                        scope.select2 = element.select2(select2Option);
+                    if (element.is('select')) {
+                        if (element.hasClass("form-control")) {
+                            element.hover(function () {
+                                scope.select2 = element.select2(select2Option);
+                                scope.select2.data("select2").$container.addClass("z-select");
+                                if(element.css("max-width") !== "none"){
+                                    scope.select2.data("select2").$container.css({maxWidth:element.css("max-width")});
+                                }
+                            })
+                        } else {
+                            scope.select2 = element.select2(select2Option);
+                        }
                     } else {
-                        throw error("此指令需是用在select标签上");
+                        throw new Error("此指令需是用在select标签上");
                     }
 
                     //$timeout(function() {
@@ -57,8 +74,8 @@
                     //    });
                     //});
 
-                    if(attrs.multiple) {
-                        if(attrs.tags==="true") {
+                    if (attrs.multiple) {
+                        if (attrs.tags === "true") {
                         } else {
                             element.parent().addClass("hidden-select2");
                         }
@@ -66,8 +83,13 @@
 
                     scope.$watch('ngModel', function () {
                         $timeout(function () {
-                            scope.select2.val(scope.ngModel);
-                            scope.select2.trigger("change");
+                            if(scope.select2) {
+                                scope.select2.val(scope.ngModel);
+                                scope.select2.trigger("change");
+                            }else{
+                                element.val(scope.ngModel);
+                                element.trigger("change");
+                            }
                         });
                     });
 
